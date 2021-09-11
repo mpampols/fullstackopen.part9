741a938efa8600e536b53c5f9b945fe1ebe9ef74
Marc Pàmpols <mpampols@gmail.com>
2021-09-11 16:06:13 +0200

    Add 9.10

--- patientor-backend/src/index.ts
+++ patientor-backend/src/index.ts
@@ -1,15 +1,19 @@
 import express from 'express';
+import diagnoseRouter from './routes/diagnoses';
+
 const app = express();
 
 app.use(express.json());
 
-const PORT = 3003
+const PORT = 3003;
 
 app.get('/ping', (_req, res) => {
-  console.log('pong')
-  res.send('pong')
-})
+  console.log('pong');
+  res.send('pong');
+});
+
+app.use('/api/diagnoses', diagnoseRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-})
+});
