f75d0ad424627a0b47a4c6824d17ef604e3fb0e8
Marc Pampols <mpampols@gmail.com>
2021-09-12 11:41:49 +0200

    Changes for 9.20

--- patientor/src/PatientListPage/index.tsx
+++ patientor/src/PatientListPage/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
-import { Container, Table, Button } from "semantic-ui-react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import { Container, Table, Button, TableCell } from "semantic-ui-react";
 
 import { PatientFormValues } from "../AddPatientModal/AddPatientForm";
 import AddPatientModal from "../AddPatientModal";
@@ -38,38 +39,42 @@ const PatientListPage = () => {
 
   return (
     <div className="App">
-      <Container textAlign="center">
-        <h3>Patient list</h3>
-      </Container>
-      <Table celled>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>Name</Table.HeaderCell>
-            <Table.HeaderCell>Gender</Table.HeaderCell>
-            <Table.HeaderCell>Occupation</Table.HeaderCell>
-            <Table.HeaderCell>Health Rating</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {Object.values(patients).map((patient: Patient) => (
-            <Table.Row key={patient.id}>
-              <Table.Cell>{patient.name}</Table.Cell>
-              <Table.Cell>{patient.gender}</Table.Cell>
-              <Table.Cell>{patient.occupation}</Table.Cell>
-              <Table.Cell>
-                <HealthRatingBar showText={false} rating={1} />
-              </Table.Cell>
+      <Router>
+        <Container textAlign="center">
+          <h3>Patient list</h3>
+        </Container>
+        <Table celled>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Name</Table.HeaderCell>
+              <Table.HeaderCell>Gender</Table.HeaderCell>
+              <Table.HeaderCell>Occupation</Table.HeaderCell>
+              <Table.HeaderCell>Health Rating</Table.HeaderCell>
+              <Table.HeaderCell>Options</Table.HeaderCell>
             </Table.Row>
-          ))}
-        </Table.Body>
-      </Table>
-      <AddPatientModal
-        modalOpen={modalOpen}
-        onSubmit={submitNewPatient}
-        error={error}
-        onClose={closeModal}
-      />
-      <Button onClick={() => openModal()}>Add New Patient</Button>
+          </Table.Header>
+          <Table.Body>
+            {Object.values(patients).map((patient: Patient) => (
+              <Table.Row key={patient.id}>
+                <Table.Cell>{patient.name}</Table.Cell>
+                <Table.Cell>{patient.gender}</Table.Cell>
+                <Table.Cell>{patient.occupation}</Table.Cell>
+                <Table.Cell>
+                  <HealthRatingBar showText={false} rating={1} />
+                </Table.Cell>
+                <TableCell><Button as={Link} to="../" primary>View</Button></TableCell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
+        <AddPatientModal
+          modalOpen={modalOpen}
+          onSubmit={submitNewPatient}
+          error={error}
+          onClose={closeModal}
+        />
+        <Button onClick={() => openModal()}>Add New Patient</Button>
+      </Router>
     </div>
   );
 };
